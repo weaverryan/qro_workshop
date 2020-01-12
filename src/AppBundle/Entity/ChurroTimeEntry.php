9a81247a280646d2a4292b55c19078f0bddeeffb
Ryan Weaver <ryan@thatsquality.com>
2020-01-12 08:38:46 -0500

    no custom repository to start

--- src/AppBundle/Entity/ChurroTimeEntry.php
+++ src/AppBundle/Entity/ChurroTimeEntry.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="AppBundle\Repository\ChurroTimeEntryRepository")
+ * @ORM\Entity()
  * @ORM\Table()
  */
 class ChurroTimeEntry
