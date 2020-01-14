93f22f37d30668f898807713dc6d61585e172e34
Ryan Weaver <ryan@thatsquality.com>
2020-01-14 15:15:31 -0600

    Step 61: UnitTest: Mock 2 dependencies

--- src/AppBundle/Tests/Service/ChurroTimeEntryStatsHelperTest.php
+++ src/AppBundle/Tests/Service/ChurroTimeEntryStatsHelperTest.php
@@ -2,12 +2,21 @@
 
 namespace AppBundle\Tests\Service;
 
+use AppBundle\Model\ChurroTypeStats;
+use AppBundle\Service\ChurroTimeEntryStatsHelper;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 class ChurroTimeEntryStatsHelperTest extends TestCase
 {
     public function testGetMostEfficientTypeData()
     {
-        
+        $registry = $this->createMock(Registry::class);
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $helper = new ChurroTimeEntryStatsHelper($registry, $logger);
+        $stats = $helper->getMostEfficientTypeData();
+        $this->assertInstanceOf(ChurroTypeStats::class, $stats);
     }
 }
