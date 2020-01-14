8ddc09051476c0f107d6915ac406d6264f807115
Ryan Weaver <ryan@thatsquality.com>
2020-01-14 15:15:21 -0600

    Step 22: Container: re-use for homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,8 +10,13 @@ class DefaultController extends Controller
 {
     public function homepageAction()
     {
-        return $this->render('AppBundle:Default:homepage.html.twig', array(
+        $statsHelper = $this->container
+            ->get('app.churro_time_entry_stats_helper');
+        $bestTypeData = $statsHelper->getMostEfficientTypeData();
 
+        return $this->render('AppBundle:Default:homepage.html.twig', array(
+            'bestType' => $bestTypeData['type'],
+            'avg' => $bestTypeData['average'],
         ));
     }
 }
