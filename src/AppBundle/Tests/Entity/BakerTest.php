f736b0a22ee75897acf2409d922e37786523ec57
Ryan Weaver <ryan@thatsquality.com>
2020-01-14 15:15:30 -0600

    Step 55: UnitTest: BakerTest bootstrap

---
+++ src/AppBundle/Tests/Entity/BakerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class BakerTest extends TestCase
+{
+    public function testGetAbbreviatedName()
+    {
+        $this->assertTrue(true);
+    }
+}
+
