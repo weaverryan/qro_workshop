378046c268a7c709879fc9fb2240446e7f79ed79
Ryan Weaver <ryan@thatsquality.com>
2020-01-14 15:15:37 -0600

    Step 86: Form: add ChoiceType

--- src/AppBundle/Form/ChurroTimeEntryForm.php
+++ src/AppBundle/Form/ChurroTimeEntryForm.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,7 +13,12 @@ class ChurroTimeEntryForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('type')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'vanilla' => 'Vanilla',
+                    'chocolate' => 'Chocolate',
+                ]
+            ])
             ->add('startCookingAt', DateTimeType::class, [
                 'widget' => 'single_text'
             ])
