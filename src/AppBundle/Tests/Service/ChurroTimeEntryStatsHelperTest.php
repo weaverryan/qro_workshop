3ab3022abc7a943bf432521e7152a4307fc73f25
Ryan Weaver <ryan@thatsquality.com>
2020-01-14 15:15:32 -0600

    Step 63: UnitTest: mock & return time entries

--- src/AppBundle/Tests/Service/ChurroTimeEntryStatsHelperTest.php
+++ src/AppBundle/Tests/Service/ChurroTimeEntryStatsHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Tests\Service;
 
+use AppBundle\Entity\ChurroTimeEntry;
 use AppBundle\Model\ChurroTypeStats;
 use AppBundle\Repository\ChurroTimeEntryRepository;
 use AppBundle\Service\ChurroTimeEntryStatsHelper;
@@ -20,6 +21,22 @@ class ChurroTimeEntryStatsHelperTest extends TestCase
         $registry->expects($this->once())
             ->method('getRepository')
             ->willReturn($repository);
+
+        $entry1 = new ChurroTimeEntry();
+        $entry1->setType('chocolate');
+        $entry1->setQuantityMade(10);
+
+        $entry2 = new ChurroTimeEntry();
+        $entry2->setType('chocolate');
+        $entry2->setQuantityMade(20);
+
+        $entry3 = new ChurroTimeEntry();
+        $entry3->setType('vanilla');
+        $entry3->setQuantityMade(14);
+
+        $repository->expects($this->once())
+            ->method('findAllDuringLastWeekOrderedNewestFirst')
+            ->willReturn([$entry1, $entry2, $entry3]);
 
         $helper = new ChurroTimeEntryStatsHelper($registry, $logger);
         $stats = $helper->getMostEfficientTypeData();
