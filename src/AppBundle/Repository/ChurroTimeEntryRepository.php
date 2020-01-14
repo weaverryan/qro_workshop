7c1624f319ff92552378a57f6975177572a2e099
Ryan Weaver <ryan@thatsquality.com>
2020-01-14 15:15:23 -0600

    Step 29: Repo: copy in query

--- src/AppBundle/Repository/ChurroTimeEntryRepository.php
+++ src/AppBundle/Repository/ChurroTimeEntryRepository.php
@@ -6,5 +6,15 @@ use Doctrine\ORM\EntityRepository;
 
 class ChurroTimeEntryRepository extends EntityRepository
 {
-
+    public function findAllDuringLastWeekOrderedNewestFirst()
+    {
+        $timeEntries = $this->doctrine
+            ->getRepository(ChurroTimeEntry::class)
+            ->createQueryBuilder('churro_time_entry')
+            ->where('churro_time_entry.startCookingAt > :date')
+            ->setParameter('date', new \DateTime('-1 week'))
+            ->orderBy('churro_time_entry.startCookingAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
 }
